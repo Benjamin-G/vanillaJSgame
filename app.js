d0dab095f467d5056177635352783f0a4151dab0
Benjamin Geyer <brgeyer49@gmail.com>
2018-04-16 16:05:14 -0400

    added game logic

--- app.js
+++ app.js
@@ -7,15 +7,55 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+  document.querySelector(`#current-${activePlayer}`).textContent = dice
+  document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`
 */
-let scores, roundScore, activePlayer, dice
 
+let scores, roundScore, activePlayer
 
 scores = [0,0]
 roundScore = 0
-activePlayer = 1
+activePlayer = 0
 
-dice = Math.floor(Math.random() * 6) + 1
+//Board reset Functions
+const resetCurrentScore = () => {
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+}
+const changeSelectedPlayer = (activePlayer) => {
+  otherPlayer = (activePlayer === 0 ) ? 1 : 0
 
-document.querySelector(`#current-${activePlayer}`).textContent = dice
-//document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`
+  document.querySelector(`.player-${otherPlayer}-panel`).classList.remove('active')
+  document.querySelector(`.player-${activePlayer}-panel`).classList.add('active')
+}
+
+//Reset Game HTML
+document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+resetCurrentScore()
+
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  // Random Number
+  const dice = Math.floor(Math.random() * 6) + 1
+
+  // Display the result
+  const diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block'
+  diceDOM.src = `dice-${dice}.png`
+
+  // Update score IF roll != 1
+  if(dice !== 1) {
+    //Add score
+    roundScore += dice
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore
+  } else {
+    //Next player
+    activePlayer = (activePlayer === 0 ) ? 1 : 0
+    roundScore = 0
+    resetCurrentScore()    
+    changeSelectedPlayer(activePlayer)
+  }
+})
